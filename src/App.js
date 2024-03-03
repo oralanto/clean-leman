2bf59afe46ab8e568101c6f370e4a99b697a1a14
oralanto <osee@staays.com>
2024-03-03 11:41:01 +0100

    feat: add footer header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import "./App.css";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main></main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
