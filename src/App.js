261c3ac9d750873770301cdeebbf2ef7d23dc146
oralanto <osee@staays.com>
2024-03-03 12:20:06 +0100

    feat: add Router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
 
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <main></main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="w-full">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            {/* <Route path="/serice-domicile" element={<Domicile />} />
+          <Route path="/serice-voiture" element={<Voiture />} />
+          <Route path="/serice-tarifs" element={<Tarifs />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 };
